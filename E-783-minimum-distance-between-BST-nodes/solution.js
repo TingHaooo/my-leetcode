46c7de40a7693026ac0a352da1d200fb1dec0345
TingHaoChang <ztingh@gmail.com>
2023-02-20 10:46:29 +0800

    feat: add 783

---
+++ E-783-minimum-distance-between-BST-nodes/solution.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var minDiffInBST = function (root) {
+  const tree = [];
+  const inOrderTraversal = (root) => {
+    if (!root) return;
+    inOrderTraversal(root.left);
+    tree.push(root.val);
+    inOrderTraversal(root.right);
+  };
+
+  let minDiff = Infinity;
+  for (let i = 1; i < tree.length; ++i) {
+    Math.min(minDiff, tree[i] - tree[i - 1]);
+  }
+
+  return minDiff;
+};
