51a10d29e177e9ec0c73dda66627984537328ba0
TingHaoChang <ztingh@gmail.com>
2023-03-24 14:21:15 +0800

    feat: add question 88

--- E-88-merge-sorted-array/solution.js
+++ E-88-merge-sorted-array/solution.js
@@ -6,33 +6,31 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
+  if (!n) return;
   if (!m) {
-    for (let k = 0; k < nums2.length; ++k) {
-      nums1[k] = nums2[k];
+    for (let i = 0; i < n; ++i) {
+      nums1[i] = nums2[i];
     }
     return;
   }
 
-  if (!n) {
-    return;
-  }
-
-  const cpNums1 = nums1.slice(0, m);
+  let copyNums1 = nums1.slice(0, m);
+  let k = 0;
   let i = 0;
   let j = 0;
-  while (i < m || j < n) {
-    if (cpNums1[i] > nums2[j]) {
-      nums1[i + j] = nums2[j];
-      j++;
+
+  while (k < m + n) {
+    let num1 = typeof copyNums1[i] === "undefined" ? Infinity : copyNums1[i];
+    let num2 = typeof nums2[j] === "undefined" ? Infinity : nums2[j];
+
+    if (num1 < num2) {
+      nums1[k] = copyNums1[i];
+      ++i;
+      ++k;
     } else {
-      nums1[i + j] = cpNums1[i];
-      i++;
+      nums1[k] = nums2[j];
+      ++j;
+      ++k;
     }
   }
-  console.log(nums1);
-  return;
 };
-
-merge([2, 5, 6, 0, 0, 0], 3, [1, 2, 3], 3);
-merge([1], 1, [], 0);
-merge([0], 0, [1], 1);
