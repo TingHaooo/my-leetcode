777fa23a3fc0f521f4bfb0c8e26556b8aab739e6
TingHaoChang <ztingh@gmail.com>
2023-08-14 17:37:09 +0800

    feat: add question 139

---
+++ M-139-word-break/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict):
+        dp = [False] * len(s)
+        for i in range(0, len(s)):
+            for word in wordDict:
+                if s[i + 1 - len(word): i + 1] == word and (i - len(word) == -1 or dp[i - len(word)]):
+                    dp[i] = True
+        print(dp)
+        return dp[-1]
+
+
+solution = Solution()
+print(solution.wordBreak("leetcode", ["leet", "code"]))
